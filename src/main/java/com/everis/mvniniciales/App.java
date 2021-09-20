0c1b9dfe84ae1831ded546c5214c7d6306b265c9
pedromunoz <56719748+pedrodnc@users.noreply.github.com>
2021-09-20 13:58:05 +0200

    commit con el resto del proyecto

---
+++ src/main/java/com/everis/mvniniciales/App.java
@@ -0,0 +1,14 @@
+package com.everis.mvniniciales;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( StringUtils.capitalize("Hello World!" ));
+    }
+}
