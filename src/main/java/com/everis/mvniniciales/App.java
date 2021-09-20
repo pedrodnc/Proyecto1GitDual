43a7c5186ae95f0dc6e71d3011042b39afb46d7a
pedromunoz <56719748+pedrodnc@users.noreply.github.com>
2021-09-20 14:00:36 +0200

    commit de modificacion main

--- src/main/java/com/everis/mvniniciales/App.java
+++ src/main/java/com/everis/mvniniciales/App.java
@@ -10,5 +10,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( StringUtils.capitalize("Hello World!" ));
+        System.out.println("Buenas, mi nombre es Pedro!");
     }
 }
